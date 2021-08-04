ca612c1e182516837c686463673b17943d17727e
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2021-08-04 23:11:22 +0200

    Add franceactu theme

---
+++ themes/franceactu/static/js/script.js
@@ -0,0 +1,41 @@
+// document.addEventListener("DOMContentLoaded", function() {
+// 	document.querySelector(".no-js").style.visibility = "visible";
+// });
+
+function changeCards(btnId, cards) {
+	document.getElementById("intro").classList.add("fadeOut")
+
+	setTimeout(function() {
+		document.getElementById("intro").classList.add("d-none")
+
+	}, 300);
+
+
+	allNavBtn = document.querySelectorAll(".nav-link");
+	allCards = document.querySelectorAll(".newscards");
+	idCards = document.querySelectorAll("." + cards);
+
+	allNavBtn.forEach(function(ele) {
+		ele.classList.remove("active");
+	});
+
+	btnId.classList.add("active")
+	
+	allCards.forEach(function(ele) {
+		ele.classList.remove("fadeIn");
+		ele.classList.add("fadeOut");
+		setTimeout(function() {
+		ele.classList.add("d-none");
+	}, 300);
+	});
+	setTimeout(function() {
+	idCards.forEach(function(ele) {
+		ele.classList.remove("fadeOut");
+		ele.classList.remove("d-none");
+		ele.classList.add("fadeIn");
+	});
+}, 300);
+}
+
+
+
